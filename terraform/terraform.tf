a0722c70037cf9616cc944e775aca0cec0bc8732
Nour Sabouny <programmer.nour@gmail.com>
2022-04-03 13:58:22 -0700

    switch to s3 backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = "~> 1.0"
+  backend "s3" {
+    bucket = "terraform"
+    key    = "wireguard"
+    region = "ap-southeast-1"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+  }
 
   required_providers {
     aws = {
